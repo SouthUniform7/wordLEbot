81b0c473c83382b46d2cfa01073b1d44c6fc879c
Patrick Walker <41069751+SouthUniform7@users.noreply.github.com>
2022-07-30 16:42:32 -0400

    service object

--- worLE.py
+++ worLE.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 browser.get('https://www.nytimes.com/games/wordle/index.html')
 

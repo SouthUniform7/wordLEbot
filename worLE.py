fa1b08660aca3cabd761a14ad919f8d9f76b9683
Patrick Walker <41069751+SouthUniform7@users.noreply.github.com>
2022-07-30 22:18:30 -0400

    full text input script done, need alg

--- worLE.py
+++ worLE.py
@@ -9,60 +9,58 @@ import time
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
 browser = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
-browser.get('https://www.nytimes.com/games/wordle/index.html')
 
-time.sleep(2)
+###########################################################
+#   GGGGG   L        OOOO    BBBB      AAA   L
+#   G        L       O    O   B   B     A  A   L
+#   G        L       O    O   BBBBB    A   A   L
+#   G  GG    L       O    O   B    B   AAAAA   L
+#   G   G    L       O    O   B    B   A    A   L
+#   GGGGG   LLLLLL  OOOO    BBBBB   A     A   LLLLLL
+###########################################################
+letters = ['s', 't', 'r', 'a', 'y']
 
-xButton = browser.find_element(By.XPATH, '/html/body/div/div[3]/div/div')
+xbase = '/html/body/div/div[1]/div/div[row]'
 
-xButton.click()
-
-time.sleep(1)
-
-ActionChains(browser) \
-    .send_keys('shits') \
-    .key_down(Keys.ENTER) \
-    .key_up(Keys.ENTER) \
-    .perform()
-
+###########################################################
 
 
+def send_word(word):
+
+    ActionChains(browser) \
+        .send_keys(word) \
+        .key_down(Keys.ENTER) \
+        .key_up(Keys.ENTER) \
+        .perform()
+    time.sleep(2)
+
+    return
+
+def solve(row = 1): 
+
+    if (row > 6):
+        print('bruh')
+    else:
+    
+        #rowpath = By.XPATH(xbase.replace('row', str(row)))
+
+        word_string = ""
 
 
-#time.sleep(100) #has to sleep or else it will try to find customer buttons before site loads 
+        #because for some reason python string .join(letters) was returning blank
+        for i in letters: 
+            word_string += i
 
-def solve(num): 
+        print(word_string)
+        send_word(word_string)
+        
+        row+=1
 
-    #html/body/whole site/board container/whole board/single row/single letter box
-
-    butt = browser.find_element()
-
-    butt.click()
-    time.sleep(1)
-
-    endpts = browser.find_element(By.XPATH, '')
-    endpts.click()
-    time.sleep(1)
-
-    pencil = browser.find_element(By.XPATH, '')
-    pencil.click()
-    #time.sleep(2)
-
-    select = Select(browser.find_element(By.NAME, ''))
-    #time.sleep(2)
-    select.select_by_visible_text('')
-    #time.sleep(2)
-
-    save = browser.find_element(By.XPATH, '')
-    save.click()
-
-    #time.sleep(2)
-
-
-    #for each page of customers
+        solve(row)
+        
+    
 
 
 def script():
@@ -71,52 +69,16 @@ def script():
 
     #html/body/whole site/board container/whole board/single row/single letter box
 
-    xbase = '/html/body/div/div[1]/div/div[row]'
-    
-    rowpath = By.XPATH('/html/body/div/div[1]/div/div[]')
+    browser.get('https://www.nytimes.com/games/wordle/index.html')
 
-    for row in range(1,7):   
-        rowpathstring = xbase.replace('row', row)
+    time.sleep(1)
 
-        rowpath = By.XPATH(rowpathstring)
+    xButton = browser.find_element(By.XPATH, '/html/body/div/div[3]/div/div')
 
-        letterstring = ''
+    xButton.click()
 
-        letterbox = By.XPATH()
+    time.sleep(1)
 
+    solve()
 
-
-
-
-
-
-        # if (page == 28):
-        #     for i in range(1, 7):
-        #         settingChange(i)
-        #         time.sleep(2)
-        #         browser.get('')
-        #         time.sleep(3)
-        #         for b in range (page-1): 
-                    
-        #             browser.find_element(By.LINK_TEXT, '').click()
-                    
-        #     page += 1
-
-        # else:
-            
-        #     i = 1
-        #     while i < 11:
-        #         settingChange(i)
-        #         browser.get('')
-        #         time.sleep(2)
-        
-        #         for b in range (page-1): 
-        #             browser.find_element(By.LINK_TEXT, '').click()
-                    
-        #         i += 1
-
-        #     page +=1
-        #     browser.find_element(By.LINK_TEXT, '').click()
-        #     time.sleep(2)
-
-#script()
+script()
